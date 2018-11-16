b73dfb38be827ea95f3f5feb4b8fc8f89b6853cc
celsom <celsomanhique97@gmail.com>
2018-11-16 15:18:18 +0200

    connecting databases

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
 const app = express();
 
 // SETTINGS
@@ -13,10 +15,17 @@ app.set('views',path.join(__dirname,'views'));
 
 // middlewares
 app.use(morgan('dev'));
+app.use(myConnection(mysql,{
+	host:'localhost',
+	user:'root',
+	password:'',
+	port:'3306',
+	database:'crudnodejsmysql'
+},'single'));
 
 // routes
 
-
+// starting the server
 app.listen(app.get('port'),() =>{
 	console.log('server is running in port 3000');
 }); 
